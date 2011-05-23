bff688b1c9672cd5724c726db3890a27b29c929d
Tim Simpson <tim.simpson@rackspace.com>
2011-05-23 16:16:15 -0500

    Checking in some changes to the tests I made after running them in another branch.

--- nova/tests/dns/fake.py
+++ nova/tests/dns/fake.py
@@ -21,40 +21,50 @@ Fakes a DNS driver to make testing easier.
 
 
 from nova.dns.driver import DnsEntryNotFound
+from nova.dns.driver import DnsZone
 
 
 class FakeDnsDriver(object):
     """Fakes a DnsDriver.  Useful for unit testing."""
 
-    def __init__(self):
+    def __init__(self, default_dns_zone=None):
+        self.default_zone = default_dns_zone or DnsZone()
         self.zones = FakeZoneHolder()
 
     def create_entry(self, entry):
         #name, content, type, dns_zone, priority=None
+        if not entry.dns_zone:
+            entry.dns_zone = self.default_zone
+        print("Creating entry for " + str(entry))
         self.zones.add_entry(entry)
 
-    def delete_entry(self, name, dns_zone=""):
+    def delete_entry(self, name, type, dns_zone=None):
+        dns_zone = dns_zone or self.default_zone
         if not self.zones.contains_entry(dns_zone=dns_zone, name=name):
             raise DnsEntryNotFound("No entry found in dns zone %s for name %s."
                                    % (dns_zone, name))
         self.zones.delete_entry(name, dns_zone)
 
-    def get_entries_by_content(self, content, dns_zone=""):
+    def get_entries_by_content(self, content, dns_zone=None):
+        dns_zone = dns_zone or self.default_zone
         zone = self.zones.get_zone(dns_zone)
         return (entry for entry in zone if entry.content == content)
 
-    def get_entries_by_name(self, name, dns_zone=""):
+    def get_entries_by_name(self, name, dns_zone=None):
+        dns_zone = dns_zone or self.default_zone
         zone = self.zones.get_zone(dns_zone)
         return (entry for entry in zone if entry.name == name)
 
-    def get_dns_zones(self):
+    def get_dns_zones(self, name=None):
         return self.zones.zone_names
 
     def modify_content(self, name, content, dns_zone):
+        dns_zone = dns_zone or self.default_zone
         entry = self.get_entries_by_name(name, dns_zone)
         entry.content = content
 
     def rename_entry(self, content, name, dns_zone):
+        dns_zone = dns_zone or self.default_zone
         entry = self.get_entries_by_content(content, dns_zone)
         entry.name = name
 
@@ -76,7 +86,8 @@ class FakeZoneHolder(object):
 
     def delete_entry(self, name, dns_zone):
         zone = self.get_zone(dns_zone)
-        self.zones[dns_zone] = [entry for entry in zone if entry.name != name]
+        self.zones[dns_zone.name] = [entry for entry in zone
+                                     if entry.name != name]
 
     def find_entry(self, dns_zone, name):
         zone = self.get_zone(dns_zone)
@@ -86,9 +97,9 @@ class FakeZoneHolder(object):
         return None
 
     def get_zone(self, dns_zone):
-        if not dns_zone in self.zones:
-            self.zones[dns_zone] = []
-        return self.zones[dns_zone]
+        if not dns_zone.name in self.zones:
+            self.zones[dns_zone.name] = []
+        return self.zones[dns_zone.name]
 
     @property
     def zone_names(self):
