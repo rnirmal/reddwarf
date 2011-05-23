b6b0ba4fa4de3d95096c163c21657fd36fbbba25
Tim Simpson <tim.simpson@rackspace.com>
2011-05-23 16:01:28 -0500

    Fixed incorrect copyright attribution.

--- nova/tests/dns/__init__.py
+++ nova/tests/dns/__init__.py
@@ -1,6 +1,7 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-#    Copyright (c) 2011 Citrix Systems, Inc.
+#    Copyright (c) 2011 Openstack, LLC.
+#    All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
