abd673b9397d21fb94c75862c69706871e6f6d40
Tim Simpson <tim.simpson@rackspace.com>
2011-06-03 10:39:07 -0500

    Added a test which confirms the DNS entry for a container is deleted after the container goes away.

--- integration/tests/dbaas/dns.py
+++ integration/tests/dbaas/dns.py
@@ -1,12 +1,15 @@
+import time
 import unittest
 
 from reddwarfclient import Dbaas
 from nova import flags
 from nova import utils
 from proboscis import test
+from proboscis.decorators import time_out
 from tests.dbaas.containers import container_info
 from tests.dbaas.containers import GROUP_START as CONTAINER_START
 from tests.dbaas.containers import GROUP_TEST
+from tests.dbaas.containers import GROUP_STOP as CONTAINER_STOP
 
 dns_driver = None
 
@@ -36,7 +39,7 @@ class Setup(unittest.TestCase):
       depends_on_groups=[CONTAINER_START],
       groups=[GROUP, GROUP_TEST],
       ignore=IGNORE)
-class ConfirmDns(unittest.TestCase):
+class WhenContainerIsCreated(unittest.TestCase):
     """Make sure the DNS name was provisioned.
 
     This class actually calls the DNS driver to confirm the entry that should
@@ -44,8 +47,7 @@ class ConfirmDns(unittest.TestCase):
 
     """
 
-    def test_dns_entry_exists(self):
-        global dns_driver
+    def test_dns_entry_should_exist(self):
         entry = container_info.expected_dns_entry()
         entries = dns_driver.get_entries_by_name(entry.name)
         if len(entries) < 1:
@@ -53,3 +55,25 @@ class ConfirmDns(unittest.TestCase):
                       " in the entries, which were as follows:"
                       + str(dns_driver.get_entries()))
         self.assertTrue(len(entries) > 0)
+
+
+@test(depends_on_classes=[Setup, WhenContainerIsCreated],
+      depends_on_groups=[CONTAINER_STOP],
+      groups=[GROUP],
+      ignore=IGNORE)
+class AfterContainerIsDestroyed(unittest.TestCase):
+    """Make sure the DNS name was provisioned.
+
+    This class actually calls the DNS driver to confirm the entry that should
+    exist for the given container does exist.
+
+    """
+
+    @time_out(30)
+    def test_dns_entry_exist_should_be_removed_shortly_thereafter(self):
+        entry = container_info.expected_dns_entry()
+
+        entries = dns_driver.get_entries_by_name(entry.name)
+        while len(entries) > 0:
+            time.sleep(1)
+            entries = dns_driver.get_entries_by_name(entry.name)
