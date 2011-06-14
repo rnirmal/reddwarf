69376765b8b69f84af449df415f464c986d8662f
Tim Simpson <tim.simpson@rackspace.com>
2011-06-13 23:48:48 -0500

    After seeing it on GitHub I just couldn't stand seeing the private SSH key checked in, even temporarily, so I'm removing it. For now we'll have to remember to generate this before running everything, and there's an error message if we forget (on the host VM at least).

---
+++ integration/vagrant/common/ssh/add_key_to_host.sh
@@ -0,0 +1,10 @@
+# This is meant to be called from the volume box.
+if [ ! -f /vagrant-common/ssh/id_rsa ] || [ ! -f /vagrant-common/ssh/id_rsa.pub ]
+then
+    echo 'Missing ssh key. Use ssh-keygen to create key at /vagrant-common/ssh/ (leave options such as passphrase empty).'
+    exit 1
+fi
+cp /vagrant-common/ssh/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
+cp /vagrant-common/ssh/id_rsa /home/vagrant/.ssh/id_rsa
+sudo cp /vagrant-common/ssh/id_rsa.pub /root/.ssh/id_rsa.pub
+sudo cp /vagrant-common/ssh/id_rsa.pub /root/.ssh/id_rsa
