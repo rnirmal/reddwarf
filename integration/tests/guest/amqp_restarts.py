4a0e302b6b37aeff9065c573087fb7eca294e222
Tim Simpson <tim.simpson@rackspace.com>
2011-12-02 09:06:47 -0600

    Changed CI script to run new Sneaky Pete test, along with other tweaks. * Reddwarf-CI runs the MySQL nova updater tests. * When the tests stop they print out the services without commas or quotes so its easy to copy and paste the strings into a terminal. * Rabbit is now started if it isn't running at the beginning of the amqp_restarts test.

--- integration/tests/guest/amqp_restarts.py
+++ integration/tests/guest/amqp_restarts.py
@@ -79,10 +79,14 @@ class Rabbit(object):
         return utils.execute(*cmd, run_as_root=True)
 
     def start(self):
-        self.run(0, "rabbitmqctl", "start_app")
+        print("Calling rabbitmqctl start_app")
+        out = self.run(0, "rabbitmqctl", "start_app")
+        print(out)
 
     def stop(self):
-        self.run(0, "rabbitmqctl", "stop_app")
+        print("Calling rabbitmqctl stop_app")
+        out = self.run(0, "rabbitmqctl", "stop_app")
+        print(out)
 
 
 @test(groups=["agent", "amqp.restarts"])
@@ -153,9 +157,10 @@ class WhenAgentRunsAsRabbitGoesUpAndDown(object):
                                   "--rabbit_reconnect_wait_time=1"])
 
     @test(depends_on=[check_agent_path_is_correct])
-    @time_out(30)
+    @time_out(60)
     def send_agent_a_message(self):
-        self.agent.start()
+        self.rabbit.start()
+        self.agent.start(time_out=30)
         result = self._send()
         assert_equal(result['status'], "good")
 
@@ -229,7 +234,7 @@ class WhenAgentRunsAsRabbitGoesUpAndDown(object):
                  % (total_seconds, self.original_mapped, mapped[-1]))
         if mapped[-1] > 30 * 1024:
             fail("Whoa, why is mapped memory = %d for procid=%d, proc= %s?"
-                 % (current_mapped, self.agent.find_proc_id(), self.agent_bin))
+                 % (mapped[-1], self.agent.find_proc_id(), self.agent_bin))
 
     @test(depends_on=[memory_should_not_increase_as_amqp_login_fails])
     def start_rabbit(self):
