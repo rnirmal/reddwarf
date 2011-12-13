e0991fbf22124b5c055d0d79e3e8007c65cd4b1f
Ed Cranford <ed.cranford@rackspace.com>
2011-12-13 13:34:04 -0600

    Added a positive test to the unit tests, and a negative to the integrations.

--- reddwarf/tests/api/test_admin.py
+++ reddwarf/tests/api/test_admin.py
@@ -63,6 +63,16 @@ class MgmtApiTest(test.TestCase):
     def test_hosts_restricted(self):
         self._test_path_restricted('hosts')
 
+    def test_images_available_to_admin(self):
+        req = webob.Request.blank(base_url + '/images/1')
+        admin_context = context.RequestContext('fake', 'fake', 
+                                              auth_token=True, is_admin=True)
+        res = req.get_response(util.wsgi_app(fake_auth_context=admin_context))
+        self.assertEqual(res.status_int, 200)
+        res_body = json.loads(res.body)
+        print res_body
+        self.assertTrue(len(res_body) == 1)
+
     def test_images_restricted(self):
         req = webob.Request.blank(base_url + '/images/1')
         res = req.get_response(util.wsgi_app(fake_auth_context=self.context))
