c36fc314f1c847b1cf40948ae0af65f527332111
Ed Cranford <ed.cranford@rackspace.com>
2011-12-13 16:39:56 -0600

    Adding more tests to the deserializer tests, and using the appropriate deserializer now.

--- reddwarf/tests/api/test_deserializer.py
+++ reddwarf/tests/api/test_deserializer.py
@@ -25,13 +25,16 @@ class DeserializationTest(unittest.TestCase):
     """Test all the xml deserialization"""
 
     def setUp(self):
-        self.deser = deserializer.ConfigXMLDeserializer()
+        self.config_deser = deserializer.ConfigXMLDeserializer()
+        self.db_deser = deserializer.DatabaseXMLDeserializer()
+        self.instance_deser = deserializer.InstanceXMLDeserializer()
+        self.user_deser = deserializer.UserXMLDeserializer()
 
     def test_config_create_single(self):
         xml = '<configs> \
                <config key="reddwarf_imageref" value="5" description="reddwarf image"/> \
                </configs>'
-        output = self.deser.create(xml)
+        output = self.config_deser.create(xml)
         expected = {'body':
                      {'configs':
                        [{'key': 'reddwarf_imageref', 'value': '5', 'description': 'reddwarf image'}]
@@ -43,7 +46,7 @@ class DeserializationTest(unittest.TestCase):
                <config key="reddwarf_imageref" value="5" description="reddwarf image"/> \
                <config key="test2" value="test2val" description="test2desc"/> \
                </configs>'
-        output = self.deser.create(xml)
+        output = self.config_deser.create(xml)
         expected = {'body':
                      {'configs':
                        [{'key': 'reddwarf_imageref', 'value': '5', 'description': 'reddwarf image'},
@@ -53,20 +56,20 @@ class DeserializationTest(unittest.TestCase):
 
     @raises(exception.BadRequest)
     def test_config_create_empty_body(self):
-        self.deser.create("")
+        self.config_deser.create("")
 
     @raises(exception.BadRequest)
     def test_config_create_invalid_xml(self):
-        self.deser.create("<configs><config></configs>")
+        self.config_deser.create("<configs><config></configs>")
 
     def test_config_create_no_configs(self):
         xml = '<config key="reddwarf_imageref" value="5" description="reddwarf image"/>'
-        output = self.deser.create(xml)
+        output = self.config_deser.create(xml)
         self.assertEqual(None, output)
 
     def test_config_update(self):
         xml = '<config key="reddwarf_imageref" value="5" description="reddwarf image"/>'
-        output = self.deser.update(xml)
+        output = self.config_deser.update(xml)
         expected = {'body':
                      {'config':
                        {'key': 'reddwarf_imageref', 'value': '5', 'description': 'reddwarf image'}
@@ -75,23 +78,46 @@ class DeserializationTest(unittest.TestCase):
 
     @raises(exception.BadRequest)
     def test_config_update_empty_body(self):
-        self.deser.create("")
+        self.config_deser.create("")
 
     @raises(exception.BadRequest)
     def test_config_update_invalid_xml(self):
-        self.deser.create("<configs><config></configs>")
+        self.config_deser.create("<configs><config></configs>")
+
+    def test_create_database(self):
+        self.db_deser.create("""
+            <Databases xmlns="http://docs.openstack.org/database/api/v1.0">
+                <Database name="dbname" character_set="utf8" collate="utf8_general_ci"/>
+            </Databases>
+            """)
+
+    @raises(exception.BadRequest)
+    def test_create_instance_fails_no_instance(self):
+        self.instance_deser.create("")
 
     @raises(exception.BadRequest)
     def test_create_database_fails_quote_in_name(self):
-        self.deser.create("""
+        self.instance_deser.create("""
             <Databases xmlns="http://docs.openstack.org/database/api/v1.0">
                 <Database name="db"name" character_set="utf8" collate="utf8_general_ci"/>
             </Databases>
             """)
 
+    def test_create_database_no_databases(self):
+        self.db_deser.create("""<Databases />""")
+
+    @raises(exception.BadRequest)
+    def test_create_instance_fails_no_volumes(self):
+        self.instance_deser.create("""
+            <instance xmlns="http://docs.openstack.org/database/api/v1.0"
+                name="testinstance" flavorRef="https://ord.databases.api.rackspacecloud.com/v1.0/1234/flavors/1">
+                <databases />
+            </instance>
+            """)
+
     @raises(exception.BadRequest)
     def test_create_instance_fails_quote_in_name(self):
-        self.deser.create("""
+        self.instance_deser.create("""
             <instance xmlns="http://docs.openstack.org/database/api/v1.0"
                 name="bad"name" flavorRef="https://ord.databases.api.rackspacecloud.com/v1.0/1234/flavors/1">
                 <databases />
@@ -99,9 +125,25 @@ class DeserializationTest(unittest.TestCase):
             </instance>
             """)
 
+    def test_create_instance_no_databases(self):
+        self.instance_deser.create("""
+            <instance xmlns="http://docs.openstack.org/database/api/v1.0"
+                name="testinstance" flavorRef="https://ord.databases.api.rackspacecloud.com/v1.0/1234/flavors/1">
+                <databases />
+                <volume size="2" />
+            </instance>
+            """)
+
+    def test_create_user(self):
+        self.user_deser.create("""
+            <users xmlns="http://docs.openstack.org/database/api/v1.0">
+                <user name="username" password="password" database="databaseA"/>
+            </users>
+            """)
+
     @raises(exception.BadRequest)
     def test_create_user_fails_quote_in_name(self):
-        self.deser.create("""
+        self.user_deser.create("""
             <users xmlns="http://docs.openstack.org/database/api/v1.0">
                 <user name="user"name" password="badUsername" database="databaseA"/>
             </users>
@@ -109,8 +151,12 @@ class DeserializationTest(unittest.TestCase):
 
     @raises(exception.BadRequest)
     def test_create_user_fails_quote_in_password(self):
-        self.deser.create("""
+        self.user_deser.create("""
             <users xmlns="http://docs.openstack.org/database/api/v1.0">
                 <user name="badPassword" password="pass"word" database="databaseA"/>
             </users>
             """)
+
+    def test_create_user_no_users(self):
+        self.user_deser.create("""<users />""")
+        
