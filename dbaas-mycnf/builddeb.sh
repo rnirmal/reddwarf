7512f2a595dae8ee373ba0f930edd72de4a6ed76
Nirmal Ranganathan <rnirmal@gmail.com>
2012-02-08 11:27:09 -0600

    Generating all the my.cnf templates during dbaas-mycnf package build

--- dbaas-mycnf/builddeb.sh
+++ dbaas-mycnf/builddeb.sh
@@ -25,5 +25,22 @@ cp -R etc $build_dir
 
 cd $build_dir
 
+# Define the various templates
+MEMSIZE=( "512M:1" "1024M:2" "2048M:4" "4096M:8" "8192M:16" "16384M:32" "32768M:64" )
+
+# Create the individual templates from the master template
+for i in "${MEMSIZE[@]}"; do
+    key=${i%%:*}
+    multiplier=${i##*:}
+    cat ../etc/my.cnf.base | while read line; do
+        if [[ `expr "$line" : ".*{.*}"` != "0" ]]; then
+            oldval=`echo $line | sed -e 's/.*{\(.*\)}.*/\1/'`
+            newval=`echo "$oldval * $multiplier" | bc`
+            line=`echo $line | sed -e "s/{$oldval}/$newval/"`
+        fi
+        echo $line >> etc/my.cnf.$key
+    done
+done
+
 # Build the package
 DEB_BUILD_OPTIONS=nocheck,nodocs dpkg-buildpackage -rfakeroot -b -uc -us -d
