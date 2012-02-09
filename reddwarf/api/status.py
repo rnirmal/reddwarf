9957988097ef2e1dc7bfa9a25333d8756c56774e
Tim Simpson <tim.simpson@rackspace.com>
2012-02-09 14:11:55 -0600

    Added some helper functions to the reddwarf.api.status module.

--- reddwarf/api/status.py
+++ reddwarf/api/status.py
@@ -13,19 +13,20 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 """
-Common database instance status code used across DBaaS API
+Common database instance status code used across DBaaS API.
 """
+import types
 
 from nova import log as logging
+from nova.api.openstack import common
 from nova.compute import power_state
-from nova.exception import InstanceNotFound
+from nova import db
 
 from reddwarf.db import api as dbapi
 from reddwarf.exception import NotFound
-from reddwarf.guest.db import models
 
 LOG = logging.getLogger('reddwarf.api.status')
-import types
+
 
 dbaas_mapping = {
     None: 'BUILD',
@@ -42,7 +43,9 @@ dbaas_mapping = {
     power_state.SUSPENDED: 'FAILED',
 }
 
+
 class InstanceStatus(object):
+    """The authoritative source of a Reddwarf Instance status."""
     
     def __init__(self, 
                 #vm_state=None,
@@ -64,18 +67,17 @@ class InstanceStatus(object):
 
 
     @staticmethod
-    def load_from_db(server):
-        try:
-            result = dbapi.guest_status_get(instance.id).state
-            return result
-        except InstanceNotFound:
-            pass
-        try:
-            local_id = dbapi.localid_from_uuid(instance.id)
-            result = dbapi.guest_status_get(local_id).state
-            return result
-        except InstanceNotFound:
-            return None
+    def load_from_db(context, instance_id):
+        """Loads everything from the database to find an InstanceStatus.
+
+        If multiple instances must be found, it's more efficient to create an
+        InstanceStatusLookup which will load all guest states in one db call
+        and and return an object which will return an InstanceStatus when given
+        an instance_ref or cloud server dictionary from the REST api code.
+
+        """
+        lookup = InstanceStatusLookup([instance_id])
+        return lookup.get_status_from_id(context, instance_id)
 
     @property
     def is_sql_running(self):
@@ -107,22 +109,51 @@ class InstanceStatus(object):
             result['updated_at'] = self.guest_status.updated_at
         return result
 
+
 class InstanceStatusLookup(object):
+    """
+    Stores several guest states to avoid looking them up each call, and can
+    quickly return InstanceStatus objects when given the compute instance
+    component.
+    """
     def __init__(self, guest_ids):
         self.local_ids = guest_ids
         lookup = dbapi.guest_status_get_list(self.local_ids).all()
         self.guest_status_mapping = dict([(r.instance_id, r) for r in lookup])
 
+    def get_status_from_id(self, context, id):
+        """Loads a compute instance ref to grab the instance status."""
+        instance_ref = db.instance_get(context, id)
+        return self.get_status_from_instance_ref(instance_ref)
+
+    def get_status_from_instance_ref(self, instance_ref):
+        """Uses a compute instance ref to grab the instance status."""
+        id = instance_ref['id']
+        power_state = instance_ref['power_state']
+        vm_state = instance_ref['vm_state']
+        status = common.status_from_state(vm_state, power_state)
+        return self.get_status_from_server_details(id, status)
+
     def get_status_from_server(self, server):
         # We're expecting the server dictionary as returned by the servers API.
-        if server['id'] not in self.local_ids:
-            raise NotFound(instance_id=server['id'])
-        guest_status = self.guest_status_mapping.get(server['id'])
+        id = server['id']
+        status = server['status']
+        return self.get_status_from_server_details(id, status)
+
+    def get_status_from_server_details(self, server_id, server_status):
+        """
+        Given a server id and its string status (the same as returned from the
+        servers API) returns an Reddwarf InstanceStatus.
+
+        Raises an exception if the server_id was not in the list of IDs used to
+        originally create this InstanceStatusLookup.
+        """
+        if server_id not in self.local_ids:
+            raise NotFound(instance_id=server_id)
+        guest_status = self.guest_status_mapping.get(server_id)
         guest_state = None
         if guest_status is not None:
                 guest_state = guest_status.state
-        server_status = server['status']
         return InstanceStatus(guest_state=guest_state,
                               guest_status=guest_status,
                               server_status=server_status)
-        
